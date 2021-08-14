452393aaa75791bbb09b14924e940edcd7d73b39
Gustave Monce <gustave.monce@outlook.com>
2021-08-14 11:14:56 +0200

    Project: Add RootAccessTestUWP

---
+++ RootAccessTestUWP/Core.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RootAccessTestUWP
+{
+	public static class Core
+	{
+		[DllImport("KERNELBASE.DLL", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern int RegQueryValueEx(UIntPtr hKey, string lpValueName, int lpReserved, out Core.RegistryValueKind lpType, IntPtr lpData, ref int lpcbData);
+
+		[DllImport("KERNELBASE.DLL", CharSet = CharSet.Unicode)]
+		private static extern int RegOpenKeyEx(UIntPtr hKey, string subKey, int ulOptions, int samDesired, out UIntPtr hkResult);
+
+		[DllImport("KERNELBASE.DLL", SetLastError = true)]
+		private static extern int RegCloseKey(UIntPtr hKey);
+
+		internal static string RegReadStringValue(UIntPtr rootKey, string keyPath, string valueName)
+		{
+			UIntPtr hKey;
+			int num = Core.RegOpenKeyEx(rootKey, keyPath, 0, Core.KEY_READ, out hKey);
+			if (num != 0)
+			{
+				throw new COMException("Failed to open registry key", num);
+			}
+			int cb = 1024;
+			IntPtr intPtr = Marshal.AllocHGlobal(cb);
+			Core.RegistryValueKind registryValueKind;
+			num = Core.RegQueryValueEx(hKey, valueName, 0, out registryValueKind, intPtr, ref cb);
+			if (num != 0)
+			{
+				throw new COMException("Failed to read registry value", num);
+			}
+			if (registryValueKind != Core.RegistryValueKind.String)
+			{
+				throw new Exception("Wrong registry value type");
+			}
+			string result = Marshal.PtrToStringUni(intPtr);
+			Core.RegCloseKey(hKey);
+			return result;
+		}
+
+		internal static uint RegReadDwordValue(UIntPtr rootKey, string keyPath, string valueName)
+		{
+			UIntPtr hKey;
+			int num = Core.RegOpenKeyEx(rootKey, keyPath, 0, Core.KEY_READ, out hKey);
+			if (num != 0)
+			{
+				throw new COMException("Failed to open registry key", num);
+			}
+			int cb = 1024;
+			IntPtr intPtr = Marshal.AllocHGlobal(cb);
+			Core.RegistryValueKind registryValueKind;
+			num = Core.RegQueryValueEx(hKey, valueName, 0, out registryValueKind, intPtr, ref cb);
+			if (num != 0)
+			{
+				throw new COMException("Failed to read registry value", num);
+			}
+			if (registryValueKind != Core.RegistryValueKind.DWord)
+			{
+				throw new Exception("Wrong registry value type");
+			}
+			uint result = (uint)Marshal.ReadInt32(intPtr);
+			Core.RegCloseKey(hKey);
+			return result;
+		}
+
+		public static UIntPtr HKEY_CLASSES_ROOT = (UIntPtr)2147483648U;
+
+		public static UIntPtr HKEY_CURRENT_USER = (UIntPtr)2147483649U;
+
+		public static UIntPtr HKEY_LOCAL_MACHINE = (UIntPtr)2147483650U;
+
+		public static UIntPtr HKEY_USERS = (UIntPtr)2147483651U;
+
+		private static int KEY_READ = 131097;
+
+		public enum RegistryValueKind
+		{
+			Unknown,
+			String,
+			ExpandString,
+			Binary,
+			DWord,
+			MultiString = 7,
+			QWord = 11,
+			None = -1
+		}
+	}
+}
---
+++ RootAccessTestUWP/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RootAccessTestUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            TextBlock.Text = "Reading: HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\PhoneProvisioner\\ColdBootDone" + Environment.NewLine + Environment.NewLine;
+
+            bool flag = false;
+            try
+            {
+                Core.RegReadDwordValue(Core.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\PhoneProvisioner", "ColdBootDone");
+                flag = true;
+            }
+            catch
+            {
+            }
+
+            if (flag)
+            {
+                TextBlock.Text = string.Concat(new string[]
+                {
+                    TextBlock.Text,
+                    "Success!",
+                    Environment.NewLine,
+                    Environment.NewLine,
+                    "Root Access: Enabled"
+                });
+            }
+            else
+            {
+                TextBlock.Text = string.Concat(new string[]
+                {
+                    TextBlock.Text,
+                    "Failed!",
+                    Environment.NewLine,
+                    Environment.NewLine,
+                    "Root Access: Disabled"
+                });
+            }
+        }
+    }
+}
