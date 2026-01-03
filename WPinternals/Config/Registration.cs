df02025a31c25fc2fd618ecf80ec348e5bb2ae10
Gustave Monce <gustave.monce@outlook.com>
2026-01-03 23:26:37 +0100

    Update timebomb

--- WPinternals/Config/Registration.cs
+++ WPinternals/Config/Registration.cs
@@ -33,7 +33,7 @@ namespace WPinternals.Config
         internal const bool IsPrerelease = false;
 #endif
 
-        internal static readonly DateTime ExpirationDate = new(2025, 12, 25);
+        internal static readonly DateTime ExpirationDate = new(2026, 4, 1);
 
         internal static void CheckExpiration()
         {
